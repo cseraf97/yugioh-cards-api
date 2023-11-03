0b4b217114d376a8aff9576b9c607fa60f443cb7
Cristian Serafini <serafinicristian97@gmail.com>
2023-11-02 23:13:07 -0300

    feat: add database module

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "It works!"
+from api.config import Config
+from api.utils.db import init_db
+
+
+
+def create_app(config):
+    """Initialize api db."""
+
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    init_db(app)
+
+    @app.route('/')
+    def _():
+        return "It works!"
+
+    return app
+
 
 if __name__ == '__main__':
-    app.run()
+    app = create_app(Config())
+    app.run(host="0.0.0.0", debug=True)
